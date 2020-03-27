7cef2fd0db034758bdd8c55262bf0cf6efffdeb5
sachingera-1901 <62735491+sachingera-1901@users.noreply.github.com>
2020-03-27 04:32:28 +0000

    Add files via upload

---
+++ PersonApplication.DataAccess/IPersonRespository.cs
@@ -0,0 +1,16 @@
+﻿using PersonApplication.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication.DataAccess
+{
+    public interface IPersonRespository
+    {
+        Task<IEnumerable<Group>> GetAllGroups();
+        Task AddPerson(Person person);
+        Task<IEnumerable<Person>> Search(string searchName, string searchGroup);
+        Task<IEnumerable<Person>> GetAllPersons();
+    }
+}

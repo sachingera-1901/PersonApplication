f2a9e29f3842c65851054e53e33f31720a66d270
Sachin Gera <sachin.gera@royalmail.com>
2020-03-27 05:07:28 +0000

    All files freshly added

---
+++ src/PersonApplication.DataAccess/PersonContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonApplication.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonApplication.DataAccess
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public PersonContext()
+           : base()
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Group> Group { get; set; }
+    }
+}
---
+++ src/PersonApplication.DataAccess/PersonRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonApplication.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PersonApplication.DataAccess
+{
+    public class PersonRepository : IPersonRespository
+    {
+        private readonly PersonContext _context;
+
+        public PersonRepository(PersonContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Group>> GetAllGroups()
+        {
+            return await _context.Group.ToListAsync();
+        }
+
+        public async Task AddPerson(Person person)
+        {
+            var group = await _context.Group.FirstAsync(x => x.Id == person.Group.Id);
+            person.Group = group;
+            _context.Add(person);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Person>> Search(string searchName, string searchGroup)
+        {
+            var persons = await _context.Person.Include(p => p.Group).ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                persons = persons.Where(s => s.Name.Contains(searchName)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(searchGroup))
+            {
+                persons = persons.Where(s => s.Group.Name.Contains(searchGroup)).ToList();
+            }
+
+            return persons;
+        }
+
+        public async Task<IEnumerable<Person>> GetAllPersons()
+        {
+            return await _context.Person.Include(p => p.Group).ToListAsync();
+        }
+    }
+}
